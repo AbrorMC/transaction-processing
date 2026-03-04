1e25eba592e69189287d280aceccc2c423caf0be
AbrorMC <mirac.9187@gmail.com>
2026-03-04 14:05:08 +0500

    fix bugs

--- src/main/java/uz/uzumtech/transaction_processing/dto/request/TransactionRequest.java
+++ src/main/java/uz/uzumtech/transaction_processing/dto/request/TransactionRequest.java
@@ -1,5 +1,6 @@
 package uz.uzumtech.transaction_processing.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -9,13 +10,37 @@ import uz.uzumtech.transaction_processing.constant.enums.TransactionType;
 import java.math.BigDecimal;
 
 public record TransactionRequest(
-        @NotNull(message = "referenceId required") Long referenceId,
-        @NotNull(message = "transaction type required") TransactionType type,
-        @NotNull(message = "amount is required") @Positive(message = "amount should be positive") BigDecimal amount,
-        @NotNull(message = "currency required") Currency currency,
-        @NotBlank(message = "senderName required") String senderName,
-        @NotBlank(message = "senderToken required") String senderToken,
-        @NotBlank(message = "receiverName required") String receiverName,
-        @NotBlank(message = "receiverToken required") String receiverToken
+        @NotNull(message = "referenceId required")
+        @JsonProperty("reference_id")
+        Long referenceId,
+
+        @NotNull(message = "transaction type required")
+        TransactionType type,
+
+        @NotNull(message = "amount is required")
+        @Positive(message = "amount should be positive") BigDecimal amount,
+
+        @NotNull(message = "currency required")
+        Currency currency,
+
+        @NotBlank(message = "senderName required")
+        @JsonProperty("sender_name")
+        String senderName,
+
+        @NotBlank(message = "senderToken required")
+        @JsonProperty("sender_token")
+        String senderToken,
+
+        @NotBlank(message = "receiverName required")
+        @JsonProperty("receiver_name")
+        String receiverName,
+
+        @NotBlank(message = "receiverToken required")
+        @JsonProperty("receiver_token")
+        String receiverToken,
+
+        @NotBlank(message = "merchantId required")
+        @JsonProperty("merchant_id")
+        String merchantId
 ) {
 }
